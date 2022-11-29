d038a6f930ee86c54b16d0e93253416fdfedc18b
marinh0w <gabrielgambardella14@gmail.com>
2022-11-29 14:39:28 -0300

    removendo configuração do domínio privado

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,12 +16,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  },
+  }/*,
   viteFinal: (config, { configType }) =={
     if (configType === 'PRODUCTION') {
       config.base = '/lab-ds/'
     }
 
     return config
-  }
+  }*/
 }
